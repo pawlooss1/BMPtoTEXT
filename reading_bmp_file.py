f4dd6fa41d682305b13dcd46e6de88e145b7b7d8
pawlooss1 <pawlooss13@gmail.com>
2019-04-06 15:08:05 +0200

    even more corrected

--- reading_bmp_file.py
+++ reading_bmp_file.py
@@ -5,6 +5,20 @@ def decode_n_bytes(image, n):
     word = image.read(n)
     return int.from_bytes(word, byteorder='little')
 
+def get_metadata(file_name):
+    image = open(file_name, 'rb')
+    trash = image.read(2)
+    size = decode_n_bytes(image, 4)
+    trash = image.read(4)
+    offset = decode_n_bytes(image, 4)
+    trash = image.read(4)
+    width = decode_n_bytes(image, 4)
+    height = decode_n_bytes(image, 4)
+    trash = image.read(2)
+    bpp = decode_n_bytes(image, 2)
+    bytes_per_pixel = bpp // 8
+    return (size, offset, width, height, bytes_per_pixel)
+
 def image_to_array(file_name):
     meta = get_metadata(file_name)
     offset = meta[1]
@@ -15,7 +29,6 @@ def image_to_array(file_name):
         fill_to_4 = 0
     else:
         fill_to_4 = 4 - (width * bpp) % 4
-    print(height, width, bpp)
     image = open(file_name, 'rb')
     trash = image.read(offset)
     array = np.empty((height, width * 2), dtype = int)
