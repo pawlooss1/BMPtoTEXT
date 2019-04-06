c70066a81d372508a2ed69573efb9f0919e446ad
pawlooss1 <pawlooss13@gmail.com>
2019-04-06 15:03:42 +0200

    Corrected reading_bmp_file.py

--- reading_bmp_file.py
+++ reading_bmp_file.py
@@ -6,25 +6,28 @@ def decode_n_bytes(image, n):
     return int.from_bytes(word, byteorder='little')
 
 def image_to_array(file_name):
-    image = open('ludzie.bmp', 'rb')
-    trash = image.read(2)
-    size = decode_n_bytes(image, 4)
-    trash = image.read(4)
-    offset = decode_n_bytes(image, 4)
-    trash = image.read(4)
-    width = decode_n_bytes(image, 4)
-    height = decode_n_bytes(image, 4)
-    trash = image.read(2)
-    bpp = decode_n_bytes(image, 2)
-    bytes_per_pixel = bpp // 8
-    image = open('ludzie.bmp', 'rb')
+    meta = get_metadata(file_name)
+    offset = meta[1]
+    width = meta[2]
+    height = meta[3]
+    bpp = meta[4]
+    if (width * bpp) % 4 == 0:
+        fill_to_4 = 0
+    else:
+        fill_to_4 = 4 - (width * bpp) % 4
+    print(height, width, bpp)
+    image = open(file_name, 'rb')
     trash = image.read(offset)
-    array = np.empty((height, width), dtype = int)
+    array = np.empty((height, width * 2), dtype = int)
     for i in range(height):
         for j in range(0, width * 2, 2):
-            byte = image.read(1)
-            number = int.from_bytes(byte, byteorder='big')
-            array[height-1-i][j] = number
-            array[height-1-i][j+1] = number
-            image.read(bytes_per_pixel - 1)
+            sum = 0
+            for k in range(bpp):
+                byte = image.read(1)
+                sum = sum + int.from_bytes(byte, byteorder='big')            
+            mean = sum / bpp
+            array[height-1-i][j] = mean
+            array[height-1-i][j+1] = mean
+        if fill_to_4 > 0:
+            trash = image.read(fill_to_4)
     return array
